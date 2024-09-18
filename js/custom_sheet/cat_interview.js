de6b1b70162c84d21db5a622a2650debeaa07c23
1F10220082 <s1f102200828@iniad.org>
2024-09-19 04:26:41 +0900

    修正

--- js/custom_sheet/cat_interview.js
+++ js/custom_sheet/cat_interview.js
@@ -5,8 +5,15 @@ const cat_interview = {
 function init(){
     document.getElementById('choice').addEventListener('change', ()=>{
         const val = document.getElementById('choice').selectedOptions[0].value;
-        window.location.href = val;
+        const safeVal = escapeHTML(val);
+        window.location.href = safeVal;
     });
+}
+
+function escapeHTML(str) {
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
 }
 
 export default cat_interview;
