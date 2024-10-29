cfb01d1e454c5ca614dac8b6356d3547b22fa926
1F10220082 <s1f102200828@iniad.org>
2024-10-29 10:39:11 +0900

    金額計算ツールに初任給への対応を追加、計算管理方法を変更

--- js/base.js
+++ js/base.js
@@ -56,6 +56,6 @@ const bundle = {
     "ESmanage": ESmanage,
     "ESdata": ESdata,
     "Index": Index,
-    "version": "2.7.9"
+    "version": "2.8.4"
 }
 export default bundle;
--- js/calc.js
+++ js/calc.js
@@ -5,15 +5,23 @@ const calc = {
 function apply_calc() {
     var capital = document.getElementById("capital");
     var sales = document.getElementById("sales");
+    var salary = document.getElementById("salary");
     var input1 = document.getElementById("1");
     var input2 = document.getElementById("2");
+    var input3 = document.getElementById("3");
     var value1 = input1.value;
     var value2 = input2.value;
-    var result = Number(value1 * 10000) + Number(value2);
+    var value3 = input3.value;
+    var result = 0;
     if (capital.checked) {
+      result = Number(value1 * 10000) + Number(value2);
       window.opener.document.getElementById("id_capital").value = result;
     } else if (sales.checked) {
+      result = Number(value1 * 10000) + Number(value2);
       window.opener.document.getElementById("id_sales_n").value = result;
+    }else if (salary.checked){
+      result = Number(value1 * 10000 *10000 ) + Number(value2 * 10000) + Number(value3 * 1000);
+      window.opener.document.getElementById("id_salary").value = result;
     }
   };
 export default calc;
